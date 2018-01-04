46ed5ecea12963212584faa4bd6ae990c00b9a4c
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 05:53:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function overwriteBestCustomer() {
   bestCustomer = "maybe bob"
 }
 
-var leastFavoriteCustomer = "Bob";
+let leastFavoriteCustomer = "Bob";
 
 function changeLeastFavoriteCustomer() {
   let leastFavoriteCustomer = "Bobby"
