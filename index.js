7c505c2e9e6131fb084068e24a40e070ddce4b7a
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 05:47:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // Write your solution in this file!
 var customerName = "bob";
 
-function upperCaseCustomerName(name) {
-   capitalizedName = customerName.toUpperCase()
-   return capitalizedName
+function upperCaseCustomerName() {
+   return customerName.toUpperCase()
  }
 
 function setBestCustomer() {
